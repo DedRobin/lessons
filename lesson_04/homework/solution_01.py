a451461bffb6c25c8cc807818c989b6eaaac5bbd
Pavel Rabekin <pavelrabekin@gmail.com>
2022-05-26 13:48:25 +0300

    1) edit files: .gitingnore (add exception - test.py) 2) add files: solution_01.py solution_02.py solution_03.py

---
+++ lesson_04/homework/solution_01.py
@@ -0,0 +1,14 @@
+"""
+Дан список my_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89], выведите все элементы, которые меньше 5.
+"""
+my_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+# 1)
+# result = [item for item in my_list if item < 5]
+# print(result)
+
+# 2)
+for item in my_list:
+    if item < 5:
+        print(item)
+
---
+++ lesson_04/homework/solution_03.py
@@ -0,0 +1,20 @@
+"""
+Написать функцию xor_cipher, принимающая 2 аргумента: строку, которую нужно зашифровать, и ключ шифрования,
+которая возвращает строку, зашифрованную путем применения функции XOR (^) над символами строки с ключом.
+Написать также функцию xor_uncipher, которая по зашифрованной строке и ключу восстанавливает исходную строку.
+"""
+
+
+def xor_cipher(string, key):
+    return "".join([chr(ord(letter) ^ key) for letter in string])
+
+
+def xor_uncipher(string, key):
+    return "".join([chr(ord(letter) ^ key) for letter in string])
+
+
+cipher_word = xor_cipher("hello", 12)
+print(cipher_word)
+
+uncipher_word = xor_uncipher(cipher_word, 12)
+print(xor_uncipher(xor_cipher("hello", 12), 12))
