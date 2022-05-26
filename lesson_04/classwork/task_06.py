6f9699ba17df4ec04eeecec6687cd8dd36479461
Pavel Rabekin <pavelrabekin@gmail.com>
2022-05-26 14:02:32 +0300

    1) edit files: task_06.py

--- lesson_04/classwork/task_06.py
+++ lesson_04/classwork/task_06.py
@@ -4,9 +4,10 @@ Task_06
 Вывести в порядке возрастания все простые числа, расположенные между n и m (включая сами числа n и m),
 а также количество x этих чисел.
 """
-
+# Variables
 start = int(input(("Enter a start number: ")))
 end = int(input(("Enter a end number: ")))
+prime_numbers = []
 
 for current_number in range(start, end + 1):
     # Number less then or equal to zero isn't prime number.
@@ -15,7 +16,8 @@ for current_number in range(start, end + 1):
 
     # Number 2 is prime number so we display it.
     elif current_number == 2:
-        print(current_number)
+        prime_numbers.append(current_number)
+        continue
 
     # Run loop which break it if number is divisible.
     for x in range(2, current_number // 2):
@@ -23,4 +25,6 @@ for current_number in range(start, end + 1):
             break
     # If loop din't find it then display "current_number".
     else:
-        print(current_number)
+        prime_numbers.append(current_number)
+
+print("Prime numbers:", *prime_numbers, "\nCounter of prime numbers: ", len(prime_numbers))
