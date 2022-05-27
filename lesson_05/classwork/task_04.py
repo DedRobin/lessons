563ab6f2f54b84bf4aa9868349273271a39785fc
Pavel Rabekin <pavelrabekin@gmail.com>
2022-05-27 21:58:05 +0300

    edit files:     task_04.py

--- lesson_05/classwork/task_04.py
+++ lesson_05/classwork/task_04.py
@@ -5,14 +5,24 @@
 
 
 def month_to_season(number_of_month):
-    if number_of_month in (1, 2, 12):
-        return "Winter"
-    elif number_of_month in (3, 4, 5):
-        return "Spring"
-    elif number_of_month in (6, 7, 8):
-        return "Summer"
-    else:
-        return "Autumn"
+    # if number_of_month in (1, 2, 12):
+    #     return "Winter"
+    # elif number_of_month in (3, 4, 5):
+    #     return "Spring"
+    # elif number_of_month in (6, 7, 8):
+    #     return "Summer"
+    # else:
+    #     return "Autumn"
+
+    dict_of_seasons = {
+        "Winter": [1, 2, 12],
+        "Spring": [3, 4, 5],
+        "Summer": [6, 7, 8],
+        "Autumn": [9, 10, 11]
+    }
+    for season, month_list in dict_of_seasons.items():
+        if number_of_month in month_list:
+            return season
 
 
 print(month_to_season(6))
