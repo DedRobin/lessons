ae2474bb0196f103318f17a5cde596b176b66cf3
Pavel Rabekin <pavelrabekin@gmail.com>
2022-05-29 19:35:45 +0300

    add lesson_05/task_02.py

--- lesson_05/homework/task_02.py
+++ lesson_05/homework/task_02.py
@@ -4,5 +4,19 @@
 Ваша программа должна используя список имен участников выдать на выходе пары, кто и кому будет дарить,
 причем сам себе человек не может подарить, дубликаты тоже не допустимы.
 """
-def secret_santa_game():
-    return
+import random
+
+
+def secret_santa(*list_of_participants):
+    list_of_participants = list(list_of_participants)
+    pairs = {}
+    while list_of_participants:
+        current_participant = random.choice(list_of_participants)
+        list_of_participants.remove(current_participant)
+        partner = random.choice(list_of_participants)
+        list_of_participants.remove(partner)
+        pairs[current_participant] = partner
+    return pairs
+
+
+print(secret_santa("Oleg", "Piter", "Albert", "Kate", "Alexandr", "Karina", "Sergei", "Ludmila"))
