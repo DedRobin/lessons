7caa0c672690ec864e71d60afc62e14326f17b8e
Pavel Rabekin <pavelrabekin@gmail.com>
2022-06-02 10:57:48 +0300

    edit files:     lessons_06:        homework:            task_01.py(use list comprehension)

--- lesson_06/homework/task_01.py
+++ lesson_06/homework/task_01.py
@@ -7,13 +7,9 @@
 def two_most_common_words(text):
     text = text.lower()
     text = text.replace(".", " ")
-    my_list = []
-    for word in set(text.split()):
-        counter = text.count(word)
-        my_list.append((counter, word))
-    most_common1, most_common2 = sorted(my_list, reverse=True)[0:2]
-    return most_common1[1], most_common2[1]
+    my_list = [(text.count(word), word) for word in set(text.split())]
+    first_most_common, second_most_common2 = sorted(my_list, reverse=True)[0:2]
+    return first_most_common[1], second_most_common2[1]
 
-
-text = "You get money to pay money for you."
+text = "You get money money to pay money for you to."
 print(two_most_common_words(text))
