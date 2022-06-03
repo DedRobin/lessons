e6d516ddefbf66ecd0af1698e6c53972a195e878
Pavel Rabekin <pavelrabekin@gmail.com>
2022-06-03 13:31:37 +0300

    edit files --> lessons_06/homework/task_01.py

--- lesson_06/homework/task_01.py
+++ lesson_06/homework/task_01.py
@@ -12,12 +12,12 @@ def two_most_common_words(text):
 
     # Find 2 most common words
     my_list = [(text.count(" " + word + " "), word) for word in set(text.split())]
-    first_most_common, second_most_common2 = sorted(my_list, reverse=True)[0:2]
+    first_most_common, second_most_common = sorted(my_list, reverse=True)[0:2]
 
     # Find longest word
     longest_word = sorted(my_list, key=lambda x: len(x[1]), reverse=True)[0][1]
 
-    return f"Two most common word: {first_most_common[1]}, {second_most_common2[1]}.\nLongest word: {longest_word}."
+    return f"Two most common word: {first_most_common[1]}, {second_most_common[1]}.\nLongest word: {longest_word}."
 
 
 text = "You get money to pay money for not unnecessary things. So you don't need to spend a lot of money!"
