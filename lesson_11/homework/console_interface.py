049e706b1cf83019a4faf70bbd614543e55476ef
Pavel Rabekin <pavelrabekin@gmail.com>
2022-06-21 14:21:12 +0300

    edit files -> lessons_11/homework/ console_interface.py(fix rendering)

--- lesson_11/homework/console_interface.py
+++ lesson_11/homework/console_interface.py
@@ -22,6 +22,78 @@ def show_table(data: list) -> None:
     print(("|" + "-" * 20) * 5 + "|")
 
 
+def menu_create_table():
+    try:
+        create_table()
+    except sqlite3.OperationalError:
+        while True:
+            print("Are you sure? Creating new table will delete old table.")
+            action = input("Selection(y/n): ")
+            if action.lower() == "y":
+                drop_table()
+                create_table()
+                print("Table is created.")
+                break
+            elif action.lower() == "n":
+                break
+            else:
+                print("incorrect choice!")
+
+
+def menu_read_data():
+    try:
+        reading_data = read_data()
+        show_table(reading_data)
+    except sqlite3.OperationalError:
+        print("No such table. Create new it.")
+
+
+def menu_insert_data():
+    while True:
+        field = input("Enter field separated by commas by pattern:\n'product name, price, quantity, comment'\n")
+        check = re.match(r"[\d\w ]+, ?\d+(\.\d+)?, ?\d+, ?[\d\w ]+", field)
+        if check:
+            field = field.split(",")
+            add_data(*field)
+            break
+        else:
+            print("Incorrect data.")
+
+
+def menu_update_data():
+    show_table(read_data())
+    id_number = int(input("Enter id: "))
+    edit_field = read_data(id_number)
+    edit_field = [list(edit_field[0])]  # [()] --> [[]] for editing
+    while True:
+        show_table(edit_field)
+        print("""What column do you want to change?
+        1) Product name;
+        2) Price;
+        3) Quantity;
+        4) Comment;
+        5) Save changes.""")
+        action = int(input("Selection: "))
+        if action == 1:
+            edit_field[0][1] = input("Enter product name: ")
+        elif action == 2:
+            edit_field[0][2] = input("Enter price: ")
+        elif action == 3:
+            edit_field[0][3] = input("Enter quantity: ")
+        elif action == 4:
+            edit_field[0][4] = input("Enter comment: ")
+        elif action == 5:
+            break
+        else:
+            print("Incorrect choice!")
+        update_data(*edit_field[0])
+
+
+def menu_remove_data():
+    id_number = int(input("Enter id: "))
+    remove_data(id_number)
+
+
 def main():
     print(("-" * 45) + "Product database" + ("-" * 45))
     while True:
@@ -35,84 +107,26 @@ def main():
         action = int(input("Selection: "))
 
         # CREATE
-
         if action == 1:
-            try:
-                create_table()
-            except sqlite3.OperationalError:
-                while True:
-                    print("Are you sure? Creating new table will delete old table.")
-                    action = input("Selection(y/n): ")
-                    if action.lower() == "y":
-                        drop_table()
-                        create_table()
-                        print("Table is created.")
-                        break
-                    elif action.lower() == "n":
-                        break
-                    else:
-                        print("incorrect choice!")
+            menu_create_table()
 
         # READ
-
         elif action == 2:
-            try:
-                reading_data = read_data()
-                show_table(reading_data)
-            except sqlite3.OperationalError:
-                print("No such table. Create new it.")
+            menu_read_data()
 
         # INSERT
-
         elif action == 3:
-            while True:
-                field = input("Enter field separated by commas by pattern:\n'product name, price, quantity, comment'\n")
-                check = re.match(r"[\d\w ]+, ?\d+(\.\d+)?, ?\d+, ?[\d\w ]+", field)
-                if check:
-                    field = field.split(",")
-                    add_data(*field)
-                    break
-                else:
-                    print("Incorrect data.")
+            menu_insert_data()
 
         # UPDATE
-
         elif action == 4:
-            show_table(read_data())
-            id_number = int(input("Enter id: "))
-            edit_field = read_data(id_number)
-            edit_field = [list(edit_field[0])]  # [()] --> [[]] for editing
-            while True:
-                show_table(edit_field)
-                print("""What column do you want to change?
-    1) Product name;
-    2) Price;
-    3) Quantity;
-    4) Comment;
-    5) Save changes.""")
-                action = int(input("Selection: "))
-                if action == 1:
-                    edit_field[0][1] = input("Enter product name: ")
-                elif action == 2:
-                    edit_field[0][2] = input("Enter price: ")
-                elif action == 3:
-                    edit_field[0][3] = input("Enter quantity: ")
-                elif action == 4:
-                    edit_field[0][4] = input("Enter comment: ")
-                elif action == 5:
-                    break
-                else:
-                    print("Incorrect choice!")
-                update_data(*edit_field[0])
+            menu_update_data()
 
         # REMOVE
-
         elif action == 5:
-            id_number = int(input("Enter id: "))
-            remove_data(id_number)
+            menu_remove_data()
 
         # EXIT
-
         elif action == 6:
             print("Exit from program.")
             break
