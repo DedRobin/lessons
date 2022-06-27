230c6868e3cbfc336b55dac067b94be31663d53e
Pavel Rabekin <pavelrabekin@gmail.com>
2022-06-27 16:20:31 +0300

    update

--- lesson_13/classwork/generate_purchase.py
+++ lesson_13/classwork/generate_purchase.py
@@ -7,21 +7,17 @@ from random import choice
 def generate_purchase(session: Session) -> None:
     fake = Faker()
 
-    users_id = [id_user for id_user in session.query(User).filter_by()]
-    products_id = [id_product.id for id_product in session.query(Product).filter_by()]
+    user = choice(session.query(User).filter_by().all())
+    product = choice(session.query(Product).filter_by().all())
     purchase = Purchase(user=user,
                         product=product,
                         purchase_quantity=fake.pyint(min_value=1, max_value=product.product_quantity)
                         )
 
-    # session.add_all()
-    # session.commit()
+    session.add(purchase)
+    session.commit()
 
 
 if __name__ == '__main__':
     test_session = create_current_session()
     generate_purchase(test_session)
-    # search = test_session.query(User).all()
-    pass
-    # for item in search:
-    #     print(item.id)
