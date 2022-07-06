f533e26231ceeb07a7d5cae3266a3b780bcd1982
Pavel Rabekin <pavelrabekin@gmail.com>
2022-07-06 22:19:18 +0300

    user -> developing update_user.py

--- lesson_13/homework/user/update_user.py
+++ lesson_13/homework/user/update_user.py
@@ -1,68 +1,136 @@
+import maskpass
+
 from lesson_13.homework.create_session import create_current_session
 from lesson_13.homework.models import User, Profile, Address
 
 
 def update_user() -> None:
     session = create_current_session()
-    run = True
-    while run:
-        print("""Menu -> Users -> Update user:
-    What column do you want to change?
-        1) Age;
-        2) Email;
-        3) Name;
-        4) Password;
-        5) Phone;
-        6) <-- Come back.""")
 
-        try:
-            selection = int(input("Selection -> "))
-        except ValueError:
-            print("Incorrect input!")
-        else:
+    buffer = {}  # buffer
 
+    # FILTER
+    filter_for_user = {}
+    filter_for_profile = {}
+    filter_for_address = {}
+
+    try:
+        user_id = int(input("Enter user ID -> "))
+
+    except ValueError:
+        print("Incorrect input!")
+    else:
+        current_user = session.query(User).filter_by(id=user_id)
+        current_profile = session.query(Profile).filter_by(user_id=user_id)
+        current_address = session.query(Address).filter_by(user_id=user_id)
+
+        run = True
+        while run:
+            print(f"""Menu -> Users -> Update user:
+            
+User - {current_user.first().email}.
+
+What column do you want to change?
+    1) Address
+    2) Age;
+    3) City;
+    4) Email;
+    5) Name;
+    6) Password;
+    7) Phone;
+    8) Save changes;
+    9) <-- Come back.""")
+            if buffer:
+                print("Your changes:")
+                for key, value in buffer.items():
+                    print(f"{key}: {value}")
             try:
-                user_id = int(input("Enter user ID -> "))
+                selection = int(input("Selection -> "))
             except ValueError:
                 print("Incorrect input!")
             else:
 
-                # AGE
+                # ADDRESS
                 if selection == 1:
-                    new_age = int(input("Enter new age -> "))
+                    previous_address = current_address.first().address
+                    new_address = input(f"Enter new address (current age = {previous_address}) -> ")
+                    filter_for_address["address"] = new_address
+                    buffer["Address"] = f"{previous_address} -> {previous_address}"
+
+                # AGE
+                elif selection == 2:
+                    previous_age = current_profile.first().age
+                    new_age = int(input(f"Enter new age (current age = {previous_age}) -> "))
+                    filter_for_profile["age"] = new_age
+                    buffer["Age"] = f"{previous_age} -> {new_age}"
+
+                # CITY
+                elif selection == 3:
+                    previous_city = current_address.first().city
+                    new_city = int(input(f"Enter new city (current city = {previous_city}) -> "))
+                    filter_for_address["city"] = new_city
+                    buffer["City"] = f"{previous_city} -> {new_city}"
 
                 # EMAIL
-                elif selection == 2:
-                    new_email = int(input("Enter new email -> "))
+                elif selection == 4:
+                    previous_email = current_user.first().email
+                    new_email = input(f"Enter new email (current email = {previous_email}) -> ")
+                    filter_for_user["email"] = new_email
+                    buffer["Email"] = f"{previous_email} -> {new_email}"
+
 
                 # NAME
-                elif selection == 3:
-                    new_name = int(input("Enter new name -> "))
+                elif selection == 5:
+                    previous_name = current_profile.first().name
+                    new_name = input(f"Enter new name (current age = {previous_name}) -> ")
+                    filter_for_profile["name"] = new_name
+                    buffer["Name"] = f"{previous_name} -> {new_name}"
 
                 # PASSWORD
-                elif selection == 4:
-                    new_password = int(input("Enter new password -> "))
+                elif selection == 6:
+                    while True:
+                        previous_password = input("Enter old password (enter 'q' for cancel) -> ")
+                        if previous_password == current_user.first().password:
+                            new_password = input("Enter new password -> ")
+                            filter_for_user["password"] = new_password
+                            buffer[f"{len(previous_password) * '*'}"] = new_password
+                            buffer["Password"] = f"{len(previous_password) * '*'} -> {len(new_password) * '*'}"
+                            break
+                        elif previous_password == "q":
+                            break
+                        else:
+                            print("Old and entered password are not match! ")
 
                 # PHONE
-                elif selection == 5:
-                    new_phone = int(input("Enter new phone -> "))
+                elif selection == 7:
+                    previous_phone = current_profile.first().phone
+                    new_phone = input(f"Enter new phone (current phone = {previous_phone}) -> ")
+                    filter_for_profile["phone"] = new_phone
+                    buffer["Phone"] = f"{previous_phone} -> {new_phone}"
 
-                # EXIT
-                elif selection == 6:
+                # SAVE
+                elif selection == 8:
+                    answer = input("Are you sure you want to apply changes?(y/n)")
+                    if answer == "y":
+                        if filter_for_address:
+                            current_address.update(filter_for_address)
+                        if filter_for_profile:
+                            current_profile.update(filter_for_profile)
+                        if filter_for_user:
+                            current_user.update(filter_for_user)
+                        session.commit()
+                        print("Changes applied.")
+                        return
+
+                # COME BACK
+                elif selection == 9:
                     run = False
 
                 else:
                     print("Incorrect input.")
 
-                current_user = session.query(User).filter_by(id=user_id)
-                current_profile = session.query(Profile).filter_by(user_id=user_id)
-                current_address = session.query(Address).filter_by(user_id=user_id)
-                # result1 = current_user.update({"email": "email"})
-                # result1 = current_profile.update({"age": 10})
-                # result1 = current_address.update({"city": "Brest"})
-                session.commit()
-                pass
-
 
 if __name__ == '__main__':
     update_user()
+    # session = create_current_session()
+    # session.query(Address).filter_by(city="Brest").first().update({"city": "Brest1"})
