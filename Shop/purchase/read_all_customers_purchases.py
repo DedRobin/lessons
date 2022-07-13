3e8ac47424f111a18e198ff8ddf0347335488583
Pavel Rabekin <pavelrabekin@gmail.com>
2022-07-13 08:39:29 +0300

    update Shop

--- Shop/purchase/read_all_customers_purchases.py
+++ Shop/purchase/read_all_customers_purchases.py
@@ -7,9 +7,11 @@ from Shop.models import *
 def read_all_customers_purchases(session: Session) -> None:
     # name = input("Enter name -> ")
     users = session.query(User).join(Purchase).filter(User.profile[0].name == "Kelly Wallace").all()
-    for u in users:
-        print(u.profile[0].name, u.purchases[0].purchase_quantity, u.purchases[0].product.product_name)
-    print()
+    if users:
+        for u in users:
+            print(u.profile[0].name, u.purchases[0].purchase_quantity, u.purchases[0].product.product_name)
+    else:
+        print(f"{name} is not find.")
 
 
 if __name__ == '__main__':
