72a80ec82ecf12d476925d8ae80bfa16ee220d52
Pavel Rabekin <pavelrabekin@gmail.com>
2022-07-13 21:45:44 +0300

    Shop/

--- lesson_13/homework/__test.py
+++
@@ -1,4 +0,0 @@
-from lesson_13.homework.models import *
-
-a = eval("Profile.name == 'Pavel Rabekin'")
-pass
--- shop/purchase/read_all_customers_purchases.py
+++ shop/purchase/read_all_customers_purchases.py
@@ -5,8 +5,8 @@ from shop.models import *
 
 
 def read_all_customers_purchases(session: Session) -> None:
-    # name = input("Enter name -> ")
-    users = session.query(User).join(Purchase).filter(User.profile[0].name == "Kelly Wallace").all()
+    name = input("Enter name -> ")
+    users = session.query(User).join(Purchase).filter(User.id > 40).all()
     if users:
         for u in users:
             print(u.profile[0].name, u.purchases[0].purchase_quantity, u.purchases[0].product.product_name)
